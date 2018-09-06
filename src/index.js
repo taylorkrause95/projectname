0b932ea845aafe559db21768b66c8adde46aa9ba
taylorkrause95 <taylor@brainstormtech.io>
2018-09-06 15:30:19 -0400

    Updating Index - not correct

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import App from './App'
+import App from './components/App'
 
 const Root = () => {
   return(
