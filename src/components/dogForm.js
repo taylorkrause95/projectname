bea002c2e1a9dd200594804db115bda92b775168
taylorkrause95 <taylor@brainstormtech.io>
2018-09-07 17:41:13 -0400

    Form

--- src/components/dogForm.js
+++ src/components/dogForm.js
@@ -1,17 +1,51 @@
 import React from 'react';
 
 class DogForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state  = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+    handleChange(event) {
+      this.setstate({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+
+
   render() {
     return (
-        <div>
-          <h1>Locations</h1>
-        <ul>
-          <li>Main Shelter</li>
-          <li>City Place Mall</li>
-          <li>Boarding</li>
-        </ul>
-      </div>
-
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Breed:
+          <input type="text" value={this.state.value} />
+          </label>
+          <label>
+          Location:
+          <input type="text" value={this.state.value} placeholder="City" />
+          </label>
+          <label>
+            Color:
+            <input type="text" value={this.state.value} />
+          </label>
+          <label>
+            Age:
+            <input type="text" value={this.state.value} />
+          </label>
+          <label>
+            Weight:
+            <input type="text" value={this.state.value} />
+          </label>
+          <label>
+          <input type="submit" value="Submit" />
+          </label>
+        </form>
     );
   }
 }
