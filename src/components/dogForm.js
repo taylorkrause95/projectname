6a5eff9ae08dbfbc8b3f789f95cb43dd1773ad6e
taylorkrause95 <41310439+taylorkrause95@users.noreply.github.com>
2018-09-08 11:40:17 -0400

    Update dogForm.js

--- src/components/dogForm.js
+++ src/components/dogForm.js
@@ -1,11 +1,51 @@
 import React from 'react';
 
 class DogForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state  = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+    handleChange(event) {
+      this.setstate({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+
+
   render() {
     return (
-        <div>
-          This is my dog form Component
-        </div>
+      <form className="dogform" onSubmit={this.handleSubmit}>
+        <label>
+          Breed:
+          <input type="text" value={this.state.value} />
+          </label>
+          <label>
+          Location:
+          <input type="text" value={this.state.value} placeholder="City" />
+          </label>
+          <label>
+            Color:
+            <input type="text" value={this.state.value} />
+          </label>
+          <label>
+            Age:
+            <input type="text" value={this.state.value} />
+          </label>
+          <label>
+            Weight:
+            <input type="text" value={this.state.value} />
+          </label>
+          <label>
+          <input type="submit" value="Submit" />
+          </label>
+        </form>
     );
   }
 }
